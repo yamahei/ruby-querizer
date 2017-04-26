436966c511b21196f63fc77fe0c1d83d02acb827
yamahei <yamaorii@gmail.com>
2017-04-26 21:03:53 +0900

    add test code.

--- test/querizerTest.rb
+++ test/querizerTest.rb
@@ -20,22 +20,23 @@ def test(q, dir)
   end
   FileUtils.mkdir_p(dir)
 
-  assert(true){
-    q.parent.insert({:num=>1, :category=>"hoge"})
-  }
-  assert(1){
-    q.parent.select.count
-  }
-  assert(1){
-    q.parent.selectByNum({:num=>1}).count
-  }
-  assert(0){
-    q.parent.selectByNum({:num=>2}).count
-  }
-  assert(1){
-    q.parent.selectByCategory({:category=>"hoge"}).count
-  }
-  assert(0){
-    q.parent.selectByCategory({:category=>"fuga"}).count
-  }
+  #insert + select
+  assert(true){ q.parent.insert({:num=>1, :category=>"hoge"}) }
+  assert(true){ q.parent.insert({:num=>2, :category=>"fuga"}) }
+  assert(2){    q.parent.select.count }
+  assert(1){    q.parent.selectByNum({:num=>1}).count  }
+  assert(0){    q.parent.selectByNum({:num=>9}).count  }
+  assert(1){    q.parent.selectByCategory({:category=>"hoge"}).count  }
+  assert(0){    q.parent.selectByCategory({:category=>"HOGE"}).count  }
+  #update + select
+  assert(true){ q.parent.update({:category=>"HOGE"},{:num=>1, :category=>"hoge"})}
+  assert(0){    q.parent.selectByCategory({:category=>"hoge"}).count  }
+  assert(1){    q.parent.selectByCategory({:category=>"HOGE"}).count  }
+  #delete + select
+  assert(true){ q.parent.delete({:num=>2}) }
+  assert(1){    q.parent.selectByCategory({:category=>"HOGE"}).count  }
+  assert(0){    q.parent.selectByCategory({:category=>"fuga"}).count  }
+  assert(1){    q.parent.selectByNum({:num=>1}).count  }
+  assert(0){    q.parent.selectByNum({:num=>2}).count  }
+
 end
